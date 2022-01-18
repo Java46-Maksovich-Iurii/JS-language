e3a4510fa567215d35d341d864251e2d18392bd1
Iurii_Maksovich <gizmaksovitch@gmail.com>
2022-01-18 23:06:38 +0200

    added ClassWork arrays methods

--- app.js
+++ app.js
@@ -1,37 +1,71 @@
-// const str1 = "abc";
-// const str2 = "abc";
-// console.log(`str1 == str2 is ${str1 == str2}`);
-// console.log(`str1 < "s" is ${str1 < "ab"}`);
-// console.log(`"123" > 23 is ${"123" > 23}`);
-// console.log(`"abc" > 23 is ${"abc" > 23}`);
-// console.log(`"abc" < 23 is ${"abc" < 23}`);
+// const ar = [2, 100, 9, 80];
+// // ar.sort(function(a, b){
+// //     return a - b;
+// // });
 
-// function stringProcessing(str) {
-//     const strP = "" + str;
-//     console.log(`${str}[4] is ${str[4]}`);
-//     console.log(`${str} length is ${str.length}`);
-//     console.log(`${str} includes "abc" is ${strP.includes("abc")}`)
-//     console.log(`substring of ${str} beginning from index 2 to index 5 is ${strP.substring(2, 5)}`);
-//     console.log(`index of "ll" in the ${str} is ${strP.indexOf("ll")}; last index of "ll" in the ${str} is ${strP.lastIndexOf("ll")} `)
+// // ar.sort((a,b) => a-b);
+// // console.log(`ar is ${ar.toString()}`);   // comparator
 
+// ar[ar.length] = 200;
+// console.log(ar);
+// ar.push(300, 25);
+// console.log(ar);
+
+// const ar1 = [-8, 30, -57];
+// // ar.push(ar1);
+// ar.push(...ar1);    // spread operator / push - to the end
+// console.log(ar);
+
+// const ar2 = [27, 35];
+// ar.unshift(...ar2); //unshift to the beginning
+// console.log(ar);
+
+// // splice - insert or replace
+// //first arg - index for insert/replacing
+// //second arg - amount of the deleted elements (if 0 only insert)
+// //third arg - what we insert (elements)
+// ar.splice(2, 0, 1, 2);
+// console.log(ar);
+
+// let el = ar.pop(); // delete and return last element
+// el = ar.shift(); // delete and return first element
+// ar.splice(3, 5);
+// console.log(ar);
+
+// const matrix1 = [[1,2,3], [4,5,6], [7,8,9], [10,11,12]];  //4 row / 3 column
+// const matrix2 = [[100,20], [50,-5], [34,28]];
+// const matrix3 = [[1,2], [3,4], [5,6]];
+// function displayMatrix(matrix) {
+//    for (let i = 0; i < matrix[i].length; i++) {
+//        let row  = "";
+//      for (let j = 0; j < matrix.length; j++){
+//         row = row + matrix[j][i] + " ";
+//      }
+//      console.log(row);
+//    }
+//    return;
 // }
+// displayMatrix(matrix3);
+// //displayMatrix(matrix2);
 
-// stringProcessing("hello world abcll");
 
-function encode(num, base) {
-    // base from 2 to 10
-    let res = "";
-    do {
-        const digit = Math.trunc(num % base);
-        const symb = getSymbol(digit);
-        res = symb + res;
-        num = Math.trunc(num / base);
-    } while(num >= 1);
-    return res;
-}
 
-function getSymbol(digit) {
-    return ""+digit;
-}
+const arHw = [13, 28, 4, 15, 25, -10, 40, 17, 27];
+arHw.sort(function(a, b) {
+   if (a%2 == 0 && b%2 == 0) {
+      if (a>b) {
+         return 1;
+      } else return -1;  
+   } 
+   if (a%2==0 && b%2==1) {
+      return -1;
+   }
+   if (a%2==1 && b%2==1) {
+      if (a<b) {
+         return 1;
+      } else return -1;
+   }
+});
+    console.log(arHw);
 
-console.log(encode(10, 2));
+

e45b735e4a79beb5e61d87d6cc91ce0baf3e52c2
Iurii_Maksovich <gizmaksovitch@gmail.com>
2022-01-18 23:07:15 +0200

    added HW#12 to app_HW file

--- app_HW.js
+++ app_HW.js
@@ -1,41 +1,36 @@
 
-function encode(num, Codingstring) {
-    let res = "";
+// #1 - sort array
 
-    if (Codingstring.length <= 1 ) {
-        console.log("Codingstring value is not correct.")
-        return;
-    }
-    if(charDoubleCheck(Codingstring) == true) {
-        console.log("Coding string has double digits.");
-        return;
-    } else {
-        do {
-            const digit = Math.trunc(num % (Codingstring.length));
-            const symb = getSymbol(digit, Codingstring);
-            res = symb + res;
-            num = Math.trunc(num / (Codingstring.length)); 
-        } while (num >= 1);
-        return res;
-    }
+const arHw = [13, 28, 4, 15, 25, -10, 40, 17, 27];
+arHw.sort(function(a, b) {
+   if (a%2 == 0 && b%2 == 0) {
+      if (a>b) {
+         return 1;
+      } else return -1;  
+   } 
+   if (a%2==0 && b%2==1) {
+      return -1;
+   }
+   if (a%2==1 && b%2==1) {
+      if (a<b) {
+         return 1;
+      } else return -1;
+   }
+});
+    console.log(arHw);
+
+ // #2 - matrixTransp
+
+
+const matrix1 = [[1,2], [3,4], [5,6]];
+function displayMatrix(matrix) {
+   for (let i = 0; i < matrix[i].length; i++) {
+       let row  = "";
+     for (let j = 0; j < matrix.length; j++){
+        row = row + matrix[j][i] + " ";
+     }
+     console.log(row);
+   }
+   return;
 }
-
-function getSymbol(digit, Codingstring) {
-    return "" + Codingstring[digit];
-}
-
-
-function charDoubleCheck(str)
-{
-for(i=0; i < str.length; i++)
-  {
-   if (str.split(str[i]).length-1 > 1) 
-    {
-        return true;
-    }  
-  }
-  return false;
-} 
-
-
-console.log(encode(5, ".-"));
+displayMatrix(matrix1);

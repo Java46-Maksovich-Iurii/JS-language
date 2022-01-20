e2079f5bd7c2cd26f24a90c1e7fa3b0bc2760c5b
Iurii_Maksovich <gizmaksovitch@gmail.com>
2022-01-20 18:49:46 +0200

    HW_updated

--- app_HW.js
+++ app_HW.js
@@ -4,33 +4,40 @@
 const arHw = [13, 28, 4, 15, 25, -10, 40, 17, 27];
 arHw.sort(function(a, b) {
    if (a%2 == 0 && b%2 == 0) {
-      if (a>b) {
-         return 1;
-      } else return -1;  
+      return a-b; 
    } 
+
    if (a%2==0 && b%2==1) {
       return -1;
    }
+
    if (a%2==1 && b%2==1) {
-      if (a<b) {
-         return 1;
-      } else return -1;
+      return b-a;
+   }
+
+   if (a%2==1 && b%2==0) {
+      return 1;
    }
 });
     console.log(arHw);
 
+
+
  // #2 - matrixTransp
 
-
 const matrix1 = [[1,2], [3,4], [5,6]];
-function displayMatrix(matrix) {
-   for (let i = 0; i < matrix[i].length; i++) {
-       let row  = "";
-     for (let j = 0; j < matrix.length; j++){
-        row = row + matrix[j][i] + " ";
-     }
-     console.log(row);
+
+function matrixTransp(matrix) {
+   const res = [];
+
+   for (let i = 0; i<matrix[0].length; i++){
+      res.push([]);
+      for (let j = 0; j<matrix.length; j++){
+         res[i].push(matrix[j][i]);
+      }
+      console.log(res[i]);
    }
    return;
 }
-displayMatrix(matrix1);
+matrixTransp(matrix1);
+

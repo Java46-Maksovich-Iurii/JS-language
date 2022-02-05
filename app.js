d99f2566100d41b42b3c30e94983070fa8ecad0e
Iurii_Maksovich <gizmaksovitch@gmail.com>
2022-02-05 16:15:06 +0200

    added CW_#17

--- app.js
+++ app.js
@@ -1,39 +1,74 @@
-const circle = {radius: 20, square: function() {
-    return 3.14 * (this.radius ** 2);
-}, perimeter: () => 2 * 3.14 * this.radius, toString: function() {
-    return `radius of this circle is ${this.radius}`}};       // NaN у стрелки нет "this", не видит радиус
-console.log(`square = ${circle.square()}, perimeter = ${circle.perimeter()}`);
-
-console.log(`circle: ${circle}`);
-
-const circle1 = {radius: 20, perimeter: function() {return 2 * 3.14 * this.radius}, toString: function() {
-    return `radius of this circle is ${this.radius}`}};
-
-// circle1.square();  error, there is no method square for circle1
-
-function Circle(radius) {
-    this.radius = radius;
-}
-Circle.prototype.square = function() {
-   return 3.14 * (this.radius ** 2);
-}
-Circle.prototype.perimeter = function() {
-    return 2 * 3.14 * this.radius;
-}
-Circle.prototype.toString = function() {
-    return `radius of this circle is ${this.radius}`
-}
-const circle10 = new Circle(10);
-
-console.log(`circle10: ${circle10}`);
-
-
-
-Array.prototype.filter = function(callbackPredicate) {
-    const res = [];
-    this.forEach((n, i, a) => !callbackPredicate(n, i, a) && res.push(n));
-    return res;
+class Person {
+    #id;
+    #name;
+    constructor(id, name) {
+        this.#id = id;
+        this.#name = name;
+    }
+    getID() {
+        return this.#id;
+    }
+    getName() {
+        return this.#name;
+    }
+    toString() {
+        return `id: ${this.#id}; name: ${this.#name};`;
+    }
 }
 
-const ar = [1,2,3,4,100];
-ar.filter(n => n % 2 === 0).forEach(n =>  console.log(n));
+const person = new Person(123, "Moshe");
+person.name = "asdd1111111111111111111s";
+console.log(`person is ${person}`);
+
+class Employee extends Person {
+    #salary;
+    constructor(id, name, salary) {
+        super(id, name);  // call constructor of Person
+        this.#salary = salary;
+    }
+    computeSalary() {
+        return this.#salary;
+    }
+    toString() {
+        return super.toString() + ` salary: ${this.computeSalary()}`
+    }
+}
+
+const employee = new Employee(234, "Sarah", 5000);
+console.log(`person2 is ${employee}`);
+console.log(typeof(employee));
+console.log(employee.constructor.name);
+
+class Child extends Person {
+    #kinderGarten
+    constructor(id, name, kinderGarten) {
+        super(id, name);
+        this.#kinderGarten = kinderGarten;
+    }
+    getKinderGarten() {
+        return this.#kinderGarten;
+    }
+    toString() {
+        return super.toString() + ` kinderGarten name is ${this.#kinderGarten}`
+    }
+}
+
+const child = new Child(345, "Jonny", "SimplDimpl");
+console.log(child.toString());
+
+class wageEmployee extends Employee {
+    #wage
+    #hours
+    constructor(id, name, salary, hours, wage){
+      super(id, name, salary);
+      this.#hours = hours;
+      this.#wage = wage; 
+    }
+    computeSalary() {
+        return super.computeSalary() + this.#hours * this.#wage;
+    }
+}
+
+const person4 = new wageEmployee (111, 'Assaf', 1000, 10 , 100);
+console.log(`person4 is ${person4}`);
+
